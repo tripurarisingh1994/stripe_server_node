90a93d7ad72bcbcba2ae6edd8c4242cd0ccb6523
TRIPURARI SINGH <tripurarisingh1994@gmail.com>
2018-05-03 16:05:15 +0530

    03-05-2018

--- api/routes/products.js
+++ api/routes/products.js
@@ -10,7 +10,7 @@ router.get('/', (req, res, next)=> {
 
 router.post('/', (req, res, next)=> {
 
-    let token = req.params.token
+    let token = req.body;
     res.status(200).json({
         message: 'hello',
         token:token
