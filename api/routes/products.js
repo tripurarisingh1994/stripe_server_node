8424b752fdd3e2046ac01e060bb5b5db7901132e
TRIPURARI SINGH <tripurarisingh1994@gmail.com>
2018-05-03 20:06:23 +0530

    03-05-2018

--- api/routes/products.js
+++ api/routes/products.js
@@ -15,21 +15,21 @@ router.post('/', (req, res, next)=> {
 
     // res.send(201, req.body);
 
-    res.status(200).json( {
-        token:stripetoken,
-        amount:amountpayable
-    })
-    // stripe.charges.create({
-    //     amount:amountpayable,
-    //     currency:'usd',
-    //     description: 'Sample transaction',
-    //     source:stripetoken
-    // }, function(err, charge) {
-    //     if (err)
-    //     res.send({error: err})
-    //     else
-    //     res.send({success: true})
+    // res.status(200).json( {
+    //     token:stripetoken,
+    //     amount:amountpayable
     // })
+    stripe.charges.create({
+        amount:amountpayable,
+        currency:'usd',
+        description: 'Sample transaction',
+        source:stripetoken
+    }, function(err, charge) {
+        if (err)
+        res.send({error: err})
+        else
+        res.send({success: true})
+    })
     
 });
 
