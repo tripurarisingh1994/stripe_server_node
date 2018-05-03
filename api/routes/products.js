5be807c7a40635f5130e39ac4c36690d87de5231
TRIPURARI SINGH <tripurarisingh1994@gmail.com>
2018-05-03 15:38:56 +0530

    03-05-2018

--- api/routes/products.js
+++ api/routes/products.js
@@ -10,7 +10,7 @@ router.get('/', (req, res, next)=> {
 
 router.post('/', (req, res, next)=> {
     res.status(200).json({
-        message: 'Hello'
+        message: req
     })
 
     var stripetoken = req.body.stripetoken
