2afdc90b504e632715f4c8ec2bbf7f5558361273
TRIPURARI SINGH <tripurarisingh1994@gmail.com>
2018-05-03 15:09:50 +0530

    03-05-2018

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,15 +1,33 @@
+const stripe = require('stripe')('sk_live_eAwrqxCKbCLwKpUFxUh9g4EE');
 const express = require('express');
+const bodyParser = require('body-parser')
+const  cors = require('cors')
+
 const router  = express.Router();
 
-router.get('/', (req, res, next)=> {
-    res.status(200).json({
-        message: 'Handling GET requests to /products'
-    })
-});
+// router.get('/', (req, res, next)=> {
+//     res.status(200).json({
+//         message: 'Handling GET requests to /products'
+//     })
+// });
 
 router.post('/', (req, res, next)=> {
-    res.status(200).json({
-        message: 'Handling POST requests to /products'
+    // res.status(200).json({
+    //     message: 'Handling POST requests to /products'
+    // })
+
+    let stripetoken = request.body.stripetoken
+    let amountpayable = request.body.amount
+    let charge = stripe.charge.create({
+        amount:amountpayable,
+        currency:'usd',
+        description: 'Sample transaction',
+        source:stripetoken
+    }, function(err, charge) {
+        if (err)
+        console.log(err)
+        else
+        response.send({success: true})
     })
 });
 
