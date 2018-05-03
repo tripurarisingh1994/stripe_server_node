c2e2d3af038b3c5179901690a8f0ddd17631fe5e
TRIPURARI SINGH <tripurarisingh1994@gmail.com>
2018-05-03 15:58:00 +0530

    03-05-2018

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const  express = require('express');
 const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const productRoutes = require('./api/routes/products');
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cors())
+
 
 app.use('/products', productRoutes);
 
